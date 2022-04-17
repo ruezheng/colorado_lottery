f0d653981f4b832b66d117dedab97c5fde2fcb04
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-17 07:04:37 -0600

    Feat: Contestent @game_interests = []

--- spec/contestant_spec.rb
+++ spec/contestant_spec.rb
@@ -16,7 +16,7 @@ describe Contestant do
 		it "has attributes" do
 			expect(alexander.full_name).to eq "Alexander Aigiades"
 			expect(alexander.age).to eq 28
-			expect(alexander.state_of_residence.to eq "CO"
+			expect(alexander.state_of_residence).to eq "CO"
 			expect(alexander.spending_money).to eq 10
 			expect(alexander.out_of_state?).to eq false
 			expect(alexander.game_interests).to eq []
@@ -25,7 +25,6 @@ describe Contestant do
 		xit "can add game intersts" do
 			alexander.add_game_interest('Mega Millions')
 			alexander.add_game_interest('Pick 4')
-
 			expect(alexander.game_interests).to eq ["Mega Millions", "Pick 4"]
 		end
 end
